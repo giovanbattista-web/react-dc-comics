d4aea1b04ad415dce167ab8ab1a5061a85b3e4be
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-06 14:20:38 +0200

    [UPD] IMG

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,23 +13,23 @@ const Main = () => {
                                    <div className="padding_foto_main flex">
                                           <div>
                                                  <img src="./public/img/buy-comics-digital-comics.png" alt="foto" />
-
+                                                 <h4>DIGITAL COMICS</h4>
                                           </div>
                                           <div>
                                                  <img src="./public/img/buy-comics-merchandise.png" alt="foto" />
-
+                                                 <h4>DC MERCHANDISE</h4>
                                           </div>
                                           <div>
                                                  <img src="./public/img/buy-comics-subscriptions.png" alt="foto" />
-
+                                                 <h4>SUBSCRIPTION</h4>
                                           </div>
                                           <div>
                                                  <img src="./public/img/buy-comics-shop-locator.png" alt="foto" />
-
+                                                 <h4>COMIC SHOP LOCATOR</h4>
                                           </div>
                                           <div>
                                                  <img src="./public/img/buy-dc-power-visa.svg" alt="foto" />
-
+                                                 <h4>DC POWER VISA</h4>
                                           </div>
                                    </div>
                             </div>
